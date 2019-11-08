a01f7178929700c64253df6aeb7758bcb96d7db3
matthiasboeker <matthias.boeker@icloud.com>
2019-11-08 16:46:40 +0100

    new

--- Timestamp_Data.py
+++ Timestamp_Data.py
@@ -13,32 +13,32 @@ import datetime
 
 col_names = ['Time','Date', 'Day-ahead Total Load Forecast', 'Total Load']
 full_dat_series = pd.read_csv('GB_Loading_Data_full_series.csv', sep =';',skiprows=[0], names=col_names )
-2016_dat_series = pd.read_csv('GB_Loading_Data_2016.csv', sep =';',skiprows=[0], names=col_names  )
-2017_dat_series = pd.read_csv('GB_Loading_Data_2017.csv', sep =';',skiprows=[0], names=col_names )
-2018_dat_series = pd.read_csv('GB_Loading_Data_2018.csv', sep =';',skiprows=[0], names=col_names )
-2019_dat_series = pd.read_csv('GB_Loading_Data_2019.csv', sep =';',skiprows=[0], names=col_names )
+dat_series_2016 = pd.read_csv('GB_Loading_Data_2016.csv', sep=';', skiprows=[0], names=col_names)
+dat_series_2017 = pd.read_csv('GB_Loading_Data_2017.csv', sep =';',skiprows=[0], names=col_names )
+dat_series_2018 = pd.read_csv('GB_Loading_Data_2018.csv', sep =';',skiprows=[0], names=col_names )
+dat_series_2019 = pd.read_csv('GB_Loading_Data_2019.csv', sep =';',skiprows=[0], names=col_names )
 
 full_dat_series.iloc[:,2:3].astype('float')
 full_dat_series.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
 full_dat_series.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
 full_dat_series = full_dat_series.iloc[:,1:]
 
-2016_dat_series.iloc[:,2:3].astype('float')
-2016_dat_series.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
-2016_dat_series.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
-2016_dat_series = full_dat_series.iloc[:,1:]
+dat_series_2016.iloc[:,2:3].astype('float')
+dat_series_2016.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
+dat_series_2016.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
+dat_series_2016 = full_dat_series.iloc[:,1:]
 
-2017_dat_series.iloc[:,2:3].astype('float')
-2017_dat_series.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
-2017_dat_series.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
-2017_dat_series = full_dat_series.iloc[:,1:]
+dat_series_2017.iloc[:,2:3].astype('float')
+dat_series_2017.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
+dat_series_2017.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
+dat_series_2017 = full_dat_series.iloc[:,1:]
 
-2018_dat_series.iloc[:,2:3].astype('float')
-2018_dat_series.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
-2018_dat_series.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
-2018_dat_series = full_dat_series.iloc[:,1:]
+dat_series_2018.iloc[:,2:3].astype('float')
+dat_series_2018.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
+dat_series_2018.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
+dat_series_2018 = full_dat_series.iloc[:,1:]
 
-2019_dat_series.iloc[:,2:3].astype('float')
-2019_dat_series.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
-2019_dat_series.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
-2019_dat_series = full_dat_series.iloc[:,1:]
+dat_series_2019.iloc[:,2:3].astype('float')
+dat_series_2019.iloc[:,1] = full_dat_series.iloc[:,1].str.cat(full_dat_series.iloc[:,0].str[8:], sep =" ")
+dat_series_2019.iloc[:,1] = pd.to_datetime(full_dat_series.iloc[:,1])
+dat_series_2019 = full_dat_series.iloc[:,1:]
